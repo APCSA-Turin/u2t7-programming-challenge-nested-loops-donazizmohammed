204a2d8abc0df906ba4eef1beaa7bf29d1a0507a
Donaziz Mohammed <donazizm@nycstudents.net>
2025-12-10 18:42:38 +0000

    wow

--- src/main/java/com/example/project/NestedLoopsRunner.java
+++ src/main/java/com/example/project/NestedLoopsRunner.java
@@ -2,7 +2,8 @@ package com.example.project;
 
 public class NestedLoopsRunner {
     public static void main(String[] args) {
-     
+      String result = NestedLoops.starStaircaseReverse(5);
+      System.out.println(result);
     }
 }
 
